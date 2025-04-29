4be9fbec1cb874ae7dd341ad51838a785559b596
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-04-29 13:46:56 +0300

    Refactor auth service injection and simplify login logic
    Updated AuthService to use forwardRef injection for UsersService to resolve circular dependency issues. Simplified `login` method by removing email verification and token generation logic. Added a console log for user details in the `login` method.

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,18 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { UsersService } from '../../users/providers/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private readonly userService: UsersService,
+  ) {}
 
   public login(email: string, password: string, id: number) {
     //check if a user exists in the database
     const user = this.userService.findOneById(id);
     //verify the user
-    if (this.isAuthenticated() && user.email == email) {
-      //generate a token
-      return 'Default Token';
-    }
+    console.log(user);
+    return this.isAuthenticated();
     //generates a token
   }
 

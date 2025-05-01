26c1b9708e35bb3cbbd4d7d2809d3a7f7c10de68
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-05-01 08:22:10 +0300

    Add TypeORM with async configuration in AppModule
    Integrated TypeORM into the application with async configuration for connecting to a PostgreSQL database. Updated dependencies in `pnpm-lock.yaml` to include packages required for TypeORM and PostgreSQL support.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,39 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    AuthModule,
+    /*TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'andy_123',
+      database: 'nestjs-blog',
+      entities: [],
+      synchronize: true,
+    })*/
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'andy_123',
+        database: 'nestjs-blog',
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

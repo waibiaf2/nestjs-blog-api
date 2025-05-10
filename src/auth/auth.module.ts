85cfc8561c5b4ba576c4bc864b7803aa9849869a
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-05-10 06:09:43 +0300

    Add `HashingProvider` and `BcryptProvider` to `AuthModule`

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './providers/auth.service';
 import { UsersModule } from '../users/users.module';
+import { HashingProvider } from './providers/hashing.provider';
+import { BcryptProvider } from './providers/bcrypt.provider';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, HashingProvider, BcryptProvider],
   imports: [forwardRef(() => UsersModule)],
   exports: [AuthService],
 })

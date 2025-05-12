af3a2f9270893876c9aa9651f5b0afacb872e214
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-05-12 04:35:49 +0300

    Set HTTP status code to 200 for `sign-in` endpoint in `AuthController`

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-in')
+  @HttpCode(HttpStatus.OK)
   public async signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }

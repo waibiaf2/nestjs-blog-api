259832f52e95b100c3c695fb4380aa2a9f77ad49
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-05-12 04:36:12 +0300

    Set explicit HTTP status code for `sign-in` endpoint in `AuthController`

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './providers/auth.service';
 import { SignInDto } from './dtos/signin.dto';
 

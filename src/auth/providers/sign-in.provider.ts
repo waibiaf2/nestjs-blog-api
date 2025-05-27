c78cffa8ae1cad9c2d5b3428cc51b70c2fa1c369
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-05-27 16:30:11 +0300

    Add refresh token support and improve password validation error handling

--- src/auth/providers/sign-in.provider.ts
+++ src/auth/providers/sign-in.provider.ts
@@ -2,7 +2,6 @@ import {
   forwardRef,
   Inject,
   Injectable,
-  RequestTimeoutException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { SignInDto } from '../dtos/signin.dto';
@@ -40,22 +39,12 @@ export class SignInProvider {
     const user = await this.userService.findOneByEmail(signInDto.email);
 
     // Compare password to the hash
-    let isPasswordValid: boolean;
-
-    try {
-      isPasswordValid = await this.hashingProvider.comparePassword(
-        signInDto.password,
-        user.password,
-      );
-    } catch (err) {
-      throw new RequestTimeoutException(err, {
-        description: 'Invalid user password, please try again.',
-      });
-    }
-
-    // Send Confirmation
-    if (!isPasswordValid)
-      throw new UnauthorizedException('Invalid user credentials');
+    const isPasswordValid = await this.hashingProvider.comparePassword(
+      signInDto.password,
+      user.password,
+    );
+    
+    if (!isPasswordValid) throw new UnauthorizedException('Invalid password');
 
     const accessToken = await this.jwtService.signAsync(
       {
--- src/config/enviroments.validation.ts
+++ src/config/enviroments.validation.ts
@@ -17,4 +17,6 @@ export default Joi.object({
   JWT_TOKEN_AUDIENCE: Joi.string().required(),
   JWT_TOKEN_ISSUER: Joi.string().required(),
   JWT_ACCESS_TOKEN_TTL: Joi.number().required(),
+  JWT_REFRESH_TOKEN_TTL: Joi.number().required(),
 });
+ 

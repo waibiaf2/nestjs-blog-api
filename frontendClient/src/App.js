407f30711d77781fdbd29f1ad4adca2f0f5889a6
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-06-08 13:25:02 +0300

    Refactor Google authentication flow and enhance UI
    - Update Google OAuth provider client ID and endpoint.
    - Wrap Google login in a styled container.
    - Add `.env` to `.gitignore` for environment configuration.
    - Introduce new styling in `App.css` for centered layout.

--- frontendClient/src/App.js
+++ frontendClient/src/App.js
@@ -1,28 +1,31 @@
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import './App.css';
 
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId="73028571094-ok0geg8ovenb4bv3ash97eighk41kb77.apps.googleusercontent.com">
-      <GoogleLogin
-        buttonText="Login"
-        onSuccess={(response) => {
-          console.log(response);
-          fetch("http://localhost:3000/auth/google-authentication", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              token: response.credential,
-            }),
-          })
-            .then((response) => console.log(response))
-            .then((data) => console.log(data));
-        }}
-      />
-    </GoogleOAuthProvider>
+    <div className="container">
+      <GoogleOAuthProvider clientId="352293678404-mcpshroa3jhnkjlarln5jbjs9mcl8h7t.apps.googleusercontent.com">
+        <GoogleLogin
+          buttonText="Login"
+          onSuccess={(response) => {
+            console.log(response);
+            fetch('http://localhost:3000/auth/google-auth', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                token: response.credential,
+              }),
+            })
+              .then((response) => console.log(response))
+              .then((data) => console.log(data));
+          }}
+        />
+      </GoogleOAuthProvider>
+    </div>
   );
 }
 

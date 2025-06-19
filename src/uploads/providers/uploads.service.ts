337e3b77942c25ea04a26c4ca8b3383b1c4760ed
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-06-19 05:04:56 +0300

    Add file upload functionality to `Uploads` module
    - Implement `uploadFile` method in `UploadsService`.
    - Create file upload endpoint in `UploadsController` with Swagger documentation.
    - Utilize `@UseInterceptors` and `FileInterceptor` for handling file uploads.
    - Add API headers for `Content-Type` and `Authorization`.

--- src/uploads/providers/uploads.service.ts
+++ src/uploads/providers/uploads.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UploadsService {}
+export class UploadsService {
+  public async uploadFile(file: Express.Multer.File) {}
+}
--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import { ApiHeaders, ApiOperation } from '@nestjs/swagger';
+import { UploadsService } from './providers/uploads.service';
 
 @Controller('uploads')
-export class UploadsController {}
+export class UploadsController {
+  constructor(private readonly uploadsService: UploadsService) {}
+
+  @UseInterceptors(FileInterceptor)
+  @ApiHeaders([
+    { name: 'Content-Type', description: 'multipart/form-data' },
+    { name: 'Authorization', description: 'Bearer Token' },
+  ])
+  @ApiOperation({ summary: 'Upload a new image to the server' })
+  @Post('file')
+  public uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.uploadsService.uploadFile(file);
+  }
+}
